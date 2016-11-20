80b8d6c387dd3e53454c7cad6d476886218dabdb
elisakis <elisa.kis@yandex.ru>
2016-11-20 20:46:12 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     setlocale (0, "Russian");
+     long n, m, l, k, r, d;
+     double q;
+     cout << "введите натуральное n\n";
+     cin >> n;
+     cout << "введите натуральное m\n";
+     cin >> m;
+     if (n > 0 && m > 0)
+     { l = n;
+       k = m;
+       if ( m > n)
+       {
+           r = n;
+           n = m;
+           m = r;
+       }
+
+       do
+       {
+               r = n/m;
+               q = n-m*r;
+               if (q != 0)
+               {
+                n = m;
+                m = q;
+               }
+               else
+               {
+                   d = l*k/m;
+               }
+
+        }
+       while (q != 0);
+       cout << "НОК n и m = " << d;
+
+     }
+     else
+     {
+         cout << "введены неверные числа";
+     }
+
+     return 0;
+ }
